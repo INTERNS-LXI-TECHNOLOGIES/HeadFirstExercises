8e8259c2da576b07a9286e1bc62a48634e4a073e
YasirHussain-Ts <yasirhussain.t.s@lxisoft.com>
2023-02-15 16:56:24 +0530

    day3

--- FourthChapter/Puzzle4.java
+++ FourthChapter/Puzzle4.java
@@ -1,39 +1,24 @@
 public class Puzzle4
 {
-    ivar;
-    factor;
-
-    public;
-    private;
-
-    Puzzle4 obs[] = new Puzzle4[6];
-
     public static void main(String[] args)
     {
-        obs[x] = new Puzzle4b(x);
-        obs[] = new Puzzle4b();
-        obs[x] = new Puzzle4b();
-        obs = new Puzzle4b();
-
+        Puzzle4b obs[] = new Puzzle4b[6];
         int y = 1;
         int x = 0;
         int result = 0;
 
         while (x < 6)
         {
-            ___________________________
-                    ___________________________
+            obs[x] = new Puzzle4b();
+            obs[x].ivar = y;
             y = y * 10;
             x = x+1;
         }
-
         x = 6;
-
         while (x > 0)
         {
-            ________
-            result = result + ___________________
             x = x-1;
+            result = result + obs[x].doStuff(x);
         }
         System.out.println("result " + result);
     }
@@ -41,34 +26,17 @@ public class Puzzle4
 
 class Puzzle4b
 {
-    ivar = x;
-    obs.ivar = x;
-    obs[x].ivar = x;
-    obs[x].ivar = y;
-
-    doStuff(x);
-    obs.doStuff(x);
-    obs[x].doStuff(factor);
-    obs[x].doStuff(x);
-
-    ivar + factor;
-    ivar * (2 + factor);
-    ivar * (5 - factor);
-    ivar * factor;
-
-    Puzzle4
-    Puzzle4b
-    Puzzle4b()
-
-    int
-    short
-
     int ivar;
-    ________ ______ doStuff(int _________) {
-    if (ivar > 100) {
-        return _________________________
-    } else {
-        return _________________________
-    }
+
+    public int doStuff(int factor)
+    {
+        if (ivar > 100)
+        {
+            return ivar * factor;
+        }
+        else
+        {
+            return ivar * (5-factor);
+        }
     }
 }
