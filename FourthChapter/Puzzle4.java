6b5573b03347fdaa7dd9837c95eef19c6e1d19b0
YasirHussain-Ts <yasirhussain.t.s@lxisoft.com>
2023-02-15 16:06:17 +0530

    day3

---
+++ FourthChapter/Puzzle4.java
@@ -0,0 +1,74 @@
+public class Puzzle4
+{
+    ivar;
+    factor;
+
+    public;
+    private;
+
+    Puzzle4 obs[] = new Puzzle4[6];
+
+    public static void main(String[] args)
+    {
+        obs[x] = new Puzzle4b(x);
+        obs[] = new Puzzle4b();
+        obs[x] = new Puzzle4b();
+        obs = new Puzzle4b();
+
+        int y = 1;
+        int x = 0;
+        int result = 0;
+
+        while (x < 6)
+        {
+            ___________________________
+                    ___________________________
+            y = y * 10;
+            x = x+1;
+        }
+
+        x = 6;
+
+        while (x > 0)
+        {
+            ________
+            result = result + ___________________
+            x = x-1;
+        }
+        System.out.println("result " + result);
+    }
+}
+
+class Puzzle4b
+{
+    ivar = x;
+    obs.ivar = x;
+    obs[x].ivar = x;
+    obs[x].ivar = y;
+
+    doStuff(x);
+    obs.doStuff(x);
+    obs[x].doStuff(factor);
+    obs[x].doStuff(x);
+
+    ivar + factor;
+    ivar * (2 + factor);
+    ivar * (5 - factor);
+    ivar * factor;
+
+    Puzzle4
+    Puzzle4b
+    Puzzle4b()
+
+    int
+    short
+
+    int ivar;
+    ________ ______ doStuff(int _________) {
+    if (ivar > 100) {
+        return _________________________
+    } else {
+        return _________________________
+    }
+    }
+}
---
+++ SecondChapter/DrumKitTestDriver.java
@@ -0,0 +1,31 @@
+class DrumKit
+{
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+    void playTopHat ()
+    {
+        System.out.println("ding ding da-ding");
+    }
+}
+
+public class DrumKitTestDriver
+{
+    public static void main(String[] args)
+    {
+        DrumKit d = new DrumKit();
+        if (d.snare == true)
+        {
+            d.playSnare();
+        }
+
+        if (d.topHat == true)
+        {
+            d.playTopHat();
+        }
+    }
+}
