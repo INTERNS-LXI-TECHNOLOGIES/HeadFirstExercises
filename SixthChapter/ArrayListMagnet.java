ddfe200cf25b7d78f026337290738b37f7571090
YasirHussain-Ts <yasirhussain.t.s@lxisoft.com>
2023-02-16 17:00:17 +0530

    Day4

---
+++ SixthChapter/ArrayListMagnet.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class ArrayListMagnet {
+    ArrayList<String> a = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        for (String element : al) {
+            a.add(0, "zero");
+            a.add(1, "one");
+            a.add(2, "two");
+            a.add(3, "three");
+        }
+        printAL(a);
+    }
+
+    public static void printAL(ArrayList<String> al)
+    {
+        if (a.contains("two"))
+        {
+            a.add("2.2");
+        }
+
+        if (a.indexOf("four") != 4)
+        {
+            a.add(4, "4.2");
+        }
+        System.out.print(element + " ");
+    }
+    System.out.println(" ");
+
+        if(a.contains("three")){
+
+    }
+
+    {
+        a.add("four");
+    }
+}
